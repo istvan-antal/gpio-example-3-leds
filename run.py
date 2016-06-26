8d8cedfcd93d090326e9d7839178b525796acf2a
István Antal <istvan.m.antal@gmail.com>
2016-06-26 16:47:28 +0100

    Added LED movement

--- run.py
+++ run.py
@@ -5,11 +5,19 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.OUT)
-
-GPIO.output(11, GPIO.HIGH)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
 try:
     while 1:
+        GPIO.output(13, GPIO.LOW)
+        GPIO.output(11, GPIO.HIGH)
+        time.sleep(100)
+        GPIO.output(11, GPIO.LOW)
+        GPIO.output(12, GPIO.HIGH)
+        time.sleep(100)
+        GPIO.output(12, GPIO.LOW)
+        GPIO.output(13, GPIO.HIGH)
         time.sleep(100)
 except KeyboardInterrupt:
     print " Quit"
